77f9549462a7852ed2afa1f1b88e3d972c9b65ce
jcamier <jcamier@gmail.com>
2018-12-31 14:11:47 -0600

    resolve file conflict

--- run_conda_py36.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# chmod +x file_name to make it executable
-#source /Users/jacquescamier/anaconda2/bin/activate py36
-python --version
-git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-
-
